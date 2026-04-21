7dcf64f28f881ede73043713071b39ad6a2205bd
Faiberxddd <ftorresgaviria@gmail.com>
2026-04-20 22:06:07 -0500

    CRUD

--- index.js
+++ index.js
@@ -61,6 +61,125 @@ app.get("/usuarios", async (req, res) => {
     })
 });
 
+//ruta para crear un nuevo usuario
+app.post("/crear", async (req, res) => {
+    //tomamos los campos de la db
+    const { nombre, apellido, telefono,  correo, rol } = req.body;
+
+    //verificamos que los campos no esten vacios
+    if (!nombre || !apellido || !telefono || !correo || !rol) {
+        console.error("❌ Faltan datos");
+        return res.status(400).json({ error: "Faltan datos" });
+    }
+
+    //insertamos los datos en la db
+    const { data, error } = await supabase
+        .from("users")
+        .insert([
+            { nombre, apellido, telefono, correo, rol }
+        ])
+        .select();
+
+    //verificamos si hay error
+    if (error) {
+        console.error("❌ Error al crear usuario:", error);
+        return res.status(500).json({ error });
+    }
+
+    //Mostrar en consola
+    console.log("✅ Usuario creado con exito");
+
+    //Respuesta al cliente (1 vez)
+    res.json({
+        mensaje: "✅ Usuario creado con exito",
+        usuarios: data[0]
+    })
+});
+
+//ruta para los datos de un db
+app.put("/usuarios/:id", async (req, res) => {
+    
+    console.log("BODY UPDATE:", req.body);
+
+    const { id } = req.params;
+    const { nombre, apellido, telefono, correo, rol } = req.body;
+
+    //validar id
+    if (!id) {
+        return res.status(400).json({ error: "Falta el id" });
+    }
+
+    //validar que llegue almenos un dato
+    if (!nombre && !apellido && !telefono && !correo && !rol) {
+        return res.status(400).json({ error: "Faltan datos" });
+    }
+    
+    //construir objeto dinamico
+    const actualizarDatos = {}
+    if (nombre) actualizarDatos.nombre = nombre;
+    if (apellido) actualizarDatos.apellido = apellido;
+    if (telefono) actualizarDatos.telefono = telefono;
+    if (correo) actualizarDatos.correo = correo;
+    if (rol) actualizarDatos.rol = rol;
+
+    //actualizar usuario
+    const { data, error } = await supabase
+        .from("users")
+        .update(actualizarDatos)
+        .eq("id", id)
+        .select();
+
+    console.log("DB", data);
+    console.log("ERROR", error);
+
+    if (error) {
+        return res.status(500).json({ error });
+    }
+
+    if (!data || data.length === 0) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.json({
+        mensaje: "✅ Datos actualizados con exito",
+        usuarios: data[0]
+    })
+});
+
+//ruta para eliminar un usuario
+app.delete("/usuarios/:id", async (req, res) => {
+
+    const { id } = req.params;
+
+    //validar id
+    if (!id) {
+        return res.status(400).json({ error: "Falta el id" });
+    }
+
+    //eliminar usuario
+    const { data, error } = await supabase
+        .from("users")
+        .delete()
+        .eq("id", id)
+        .select();
+    
+    console.log("DB", data);
+    console.log("ERROR", error);
+
+    if (error) {
+        return res.status(500).json({ error });
+    }
+
+    if (!data || data.length === 0) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.json({
+        mensaje: "✅ Usuario eliminado con exito",
+        usuarios: data[0]
+    })
+});
+
 //definimos el puerto de nuestro servidor
 const PORT = 3000;
 app.listen(PORT, () => {
