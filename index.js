87887537219a68c68a68aa6e9b0102cfca3da317
Faiberxddd <ftorresgaviria@gmail.com>
2026-04-24 20:54:27 -0500

    CRUD Tabla Facturas

--- index.js
+++ index.js
@@ -335,6 +335,160 @@ app.delete("/pedidos/:id", async (req, res) => {
         pedidos: data[0]
     })
 });
+
+
+app.get("/facturas/:id", async (req, res) => {
+    const { id } = req.params;
+    const { data, error } = await supabase
+        .from("facturas")
+        .select("*")
+        .eq("id", id)
+
+    if (error) {
+        console.error("❌ Error al obtener la factura:", error);
+        return res.status(500).json({ error });
+    }
+
+    //Mostrar en consola
+    console.log("✅ Facturas obtenida:", data);
+
+    //Respuesta al cliente (1 vez)
+    res.json({
+        facturas: data
+    })
+});
+
+//ruta para crear un nuevo pedido
+app.post("/facturas", async (req, res) => {
+    //tomamos los campos de la db
+    const { numero_factura, usuario_id, pedido_id, fecha_factura, subtotal, impuesto, total, estado, metodo_pago } = req.body;
+
+    //verificamos que los campos no esten vacios
+    if (!numero_factura || !usuario_id || !pedido_id || !fecha_factura || !subtotal || !impuesto || !total || !estado || !metodo_pago) {
+        console.error("❌ Faltan datos");
+        return res.status(400).json({ error: "Faltan datos" });
+    }
+
+    //insertamos los datos en la db
+    const { data, error } = await supabase
+        .from("facturas")
+        .insert([
+            { numero_factura, usuario_id, pedido_id, fecha_factura, subtotal, impuesto, total, estado, metodo_pago }
+        ])
+        .select();
+
+    //verificamos si hay error
+    if (error) {
+        console.error("❌ Error al crear factura:", error);
+        return res.status(500).json({ error });
+    }
+
+    //Mostrar en consola
+    console.log("✅ Factura creada con exito");
+
+    //Respuesta al cliente (1 vez)
+    res.json({
+        mensaje: "✅ Factura creada con exito",
+        facturas: data[0]
+    })
+});
+
+//ruta para los datos de un db
+app.put("/facturas/:id", async (req, res) => {
+    
+    console.log("BODY UPDATE:", req.body);
+
+    const { id } = req.params;
+    const { numero_factura, usuario_id, pedido_id, fecha_factura, subtotal, impuesto, total, estado, metodo_pago } = req.body;
+
+    //validar id
+    if (!id) {
+        return res.status(400).json({ error: "Falta el id" });
+    }
+
+    //validar que llegue almenos un dato
+    if (!numero_factura && !usuario_id && !pedido_id && !fecha_factura && !subtotal && !impuesto && !total && !estado && !metodo_pago) {
+        return res.status(400).json({ error: "Faltan datos" });
+    }
+    
+    //construir objeto dinamico
+    const actualizarDatos = {}
+    if (numero_factura) actualizarDatos.numero_factura = numero_factura;
+    if (usuario_id) actualizarDatos.usuario_id = usuario_id;
+    if (pedido_id) actualizarDatos.pedido_id = pedido_id;
+    if (fecha_factura) actualizarDatos.fecha_factura = fecha_factura;
+    if (subtotal) actualizarDatos.subtotal = subtotal;
+    if (impuesto) actualizarDatos.impuesto = impuesto;
+    if (total) actualizarDatos.total = total;
+    if (estado) actualizarDatos.estado = estado;
+    if (metodo_pago) actualizarDatos.metodo_pago = metodo_pago;
+    //actualizar factura
+    const { data, error } = await supabase
+        .from("facturas")
+        .update(actualizarDatos)
+        .eq("id", id)
+        .select();
+
+    console.log("DB", data);
+    if (error) {
+        console.error("❌ Error:", error);
+    } else {
+        console.log("✅ Modificacion exitosa");
+    }
+
+    if (error) {
+        return res.status(500).json({ error });
+    }
+
+    if (!data || data.length === 0) {
+        return res.status(404).json({ error: "Factura no encontrada" });
+    }
+
+    res.json({
+        mensaje: "✅ Datos actualizados con exito",
+        facturas: data[0]
+    })
+});
+
+//ruta para eliminar una factura
+app.delete("/facturas/:id", async (req, res) => {
+
+    const { id } = req.params;
+
+    //validar id
+    if (!id) {
+        return res.status(400).json({ error: "Falta el id" });
+    }
+
+    //eliminar factura
+    const { data, error } = await supabase
+        .from("facturas")
+        .delete()
+        .eq("id", id)
+        .select();
+    
+    console.log("DB", data);
+    if (error) {
+        console.error("❌ Error:", error);
+    } else {
+        console.log("✅ Eliminación exitosa");
+    }
+
+    if (error) {
+        return res.status(500).json({ error });
+    }
+
+    if (!data || data.length === 0) {
+        return res.status(404).json({ error: "Factura no encontrada" });
+    }
+
+    res.json({
+        mensaje: "✅ Factura eliminada con exito",
+        facturas: data[0]
+    })
+});
+
+
 //definimos el puerto de nuestro servidor
 const PORT = 3000;
 app.listen(PORT, () => {
